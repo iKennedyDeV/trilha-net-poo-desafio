221eb81d2762b959088568586f37a0c91f6f9e9d
Renan kenedy da silva <115326944+iKennedyDeV@users.noreply.github.com>
2024-01-05 12:49:06 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+using projeto_poo.obj.models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "1111111",memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("GitHub");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "22222",memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Doulingo");
+
